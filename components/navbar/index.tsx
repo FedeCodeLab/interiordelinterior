b353403ad0c6faa714036123856674d0b4951225
fedecodelab <federicoguzman.css@gmail.com>
2025-12-18 22:02:31 -0300

    Cambio de tamaño de navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
   return (
     <nav className="border-b border-[#2E2F30]">
-      <div className="flex items-center justify-between max-w-[80%] mx-auto px-6 py-4">
+      <div className="flex items-center justify-between w-[90%] md:w-[80%] mx-auto px-6 py-4">
         <h4 className="text-lg shine-text">@elinteriordelinterior</h4>
         <div className="flex items-center justify-center gap-2"></div>
       </div>
