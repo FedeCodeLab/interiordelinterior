4f42a3a44bd488ccb2f69f17f4b4fc0872767fd5
fedecodelab <federicoguzman.css@gmail.com>
2025-12-18 22:01:03 -0300

    Se cambio el tamaño del layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import posts from "@/data/posts.json";
 
 export default function Home() {
   return (
-    <main className="min-h-screen px-6 max-w-[80%] mx-auto">
+    <main className="min-h-screen px-6 w-[90%] md:w-[80%] mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-6 gap-6 auto-rows-auto [&>*:nth-child(4n-3)]:md:col-span-3 [&>*:nth-child(4n-2)]:md:col-span-3 [&>*:nth-child(4n-1)]:md:col-span-2 [&>*:nth-child(4n)]:md:col-span-4">
         {posts.map((post) => (
           <Card
